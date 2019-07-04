6d04add8b3d9c883f5c968d859de8134853216bb
ITSupport <ITSupport@MDTR30.mlad.in>
2019-07-04 12:11:04 +0100

    First commit

---
+++ Maven-Test/src/main/java/com/selenium/Testng/Maven_Test/MavenTestExercise.java
@@ -0,0 +1,18 @@
+package com.selenium.Testng.Maven_Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenTestExercise {
+
+	public static void main(String[] args) {
+		
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Desktop\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://connect.maveric-systems.com/");
+
+
+	}
+
+}
